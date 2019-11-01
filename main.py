313bd62e3298aa0a31f10d29128129e0bf6a5e57
Frank Matranga <thefrankmatranga@gmail.com>
2019-10-31 23:54:06 -0400

    Start of student overview

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
-from flask import Flask, escape, request, render_template
+import os
+
+from flask import Flask, escape, request, url_for, redirect, render_template, escape, session
 
 from csv import DictReader
 
 app = Flask(__name__)
 
-schools = []
-with open('data/schools.csv') as f:
-    for row in DictReader(f):
-        schools.append(row)
-
-classes = []
-with open('data/classes.csv') as f:
-    for row in DictReader(f):
-        classes.append(row)
+app.secret_key = os.environ['SECRET_KEY']
 
 @app.route('/')
 def homepage():
-    name = request.args.get("name", "World")
-    print(schools)
-    return render_template('pages/homepage.html')
+    if True: # if logged in
+        return render_template('pages/student_homepage.html', student={ 'first_name': 'Frank', 'last_name': 'Matranga' }, school={ 'name': 'Test School' })
+    else:
+        return render_template('pages/homepage.html')
 
-@app.route('/school/<int:school_id>')
-def school_overview(school_id):
-    # Find school
-    school = None
-    for s in schools:
-        if int(s['id']) == school_id:
-            school = s
-            break
-    
-    return render_template('pages/school_overview.html', school=school)
+# ---------- AUTHENTICATION ----------
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'GET':
+#         return render_template('pages/login.html')
+#     else:
+#         # Login form submitted
+#         session['student_id'] = request.form['student_id']
+
+#         # Find student
+#         student = None
+#         for s in students:
+#             if s['id'] == request.form['student_id']:
+#                 student = s
+#                 break
+
+#         session['student'] = student
+
+#         return redirect(url_for('homepage'))
+
+# @app.route('/logout')
+# def logout():
+#     # remove the username from the session if it's there
+#     session.pop('student_id', None)
+#     session.pop('student', None)
+#     return redirect(url_for('homepage'))
+# ------------------------------------
+
+# ------------ ERROR HANDLING --------
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     return render_template('pages/not_found.html'), 404
+# ------------------------------------
