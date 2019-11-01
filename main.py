80d4405506b09bbc1da2a312dc9e9b3364644b76
Frank Matranga <thefrankmatranga@gmail.com>
2019-10-31 20:41:26 -0400

    Start of test data

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from flask import Flask, escape, request, render_template
 
+from csv import DictReader
+
 app = Flask(__name__)
 
+schools = []
+with open('data/schools.csv') as f:
+    for row in DictReader(f):
+        schools.append(row)
+
+classes = []
+with open('data/classes.csv') as f:
+    for row in DictReader(f):
+        classes.append(row)
+
 @app.route('/')
-def hello():
+def homepage():
     name = request.args.get("name", "World")
+    print(schools)
     return render_template('pages/homepage.html')
+
+@app.route('/school/<int:school_id>')
+def school_overview(school_id):
+    # Find school
+    school = None
+    for s in schools:
+        if int(s['id']) == school_id:
+            school = s
+            break
+    
+    return render_template('pages/school_overview.html', school=school)
